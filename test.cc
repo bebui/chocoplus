1c5a650de870a79beee093df6a1e336ca79f21c4
Julien Menana <jmenana@gmail.com>
2009-02-16 18:00:52 +0100

    test with bitset

--- test.cc
+++ test.cc
@@ -4,18 +4,39 @@
 #include "memory/environment.h"
 #include "solver/solver.h"
 #include <iostream>
+#include <boost/dynamic_bitset.hpp>
+
 
 using namespace std;
 int main()
 {
   
   Solver s;
+  
+  
+  
+  
+  boost::dynamic_bitset<StoredLong> x(5); // all 0's by default
+  x[0] = 1;
+  x[1] = 1;
+  x[4] = 1;
+  for (boost::dynamic_bitset<StoredLong>::size_type i = 0; i < x.size(); ++i)
+    std::cout << x[i];
+  std::cout << "\n";
+  std::cout << x << "\n";
+  
+  
+  
+  
+  
+  
   StoredInt si(10);
   StoredLong sl(9999394);
   StoredLong sl2(6);
   StoredLong sl3;
+  
   sl3 = sl|sl2;
-  cout << (StoredLong(12) > 0) << endl;
+  cout << ((StoredLong (12)) > 0) << endl;
   s.push();
   si.set(255);
   sl.set(245245245);
