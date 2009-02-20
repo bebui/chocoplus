f6bd7d0d0adf7022d7af7f9afe5549b0cec6ffb7
Julien Menana <jmenana@gmail.com>
2009-02-20 16:09:26 +0100

    nouveaux tests

--- test.cc
+++ test.cc
@@ -8,24 +8,21 @@ using namespace std;
 int main()
 {
   
-  Solver s;
-  
+    Solver s;
 
-  s.push();
-  IntVar a = s.make_var("a",10,12);
-  IntVar b = s.make_var("b",10,11);
-  
-  //Constraint c = s.eq(a,b);
-  Constraint c2 = s.neq(a,b);
-  
-  //s.post(c);
-  s.post(c2);
-  cout << ((s.solve()==0)?"NO SOLUTION" : "SOLVED") << endl;
-//  b.update_inf(10);
-  cout << a.str() << endl << b.str() << endl; 
-    
+
+    s.push();
+    IntVar a = s.make_var("a",10,12);
+    IntVar b = s.make_var("b",10,11);
+
+    Constraint c = s.eq(12,a);
+    Constraint c2 = s.neq(10,b);
+
+    s.post(c);
+    s.post(c2);
+    cout << ((s.solve()==0)?"NO SOLUTION" : "SOLVED") << endl;
+  //  b.update_inf(10);
+    cout << a.str() << endl << b.str() << endl; 
  
-  
-  
   
 }
