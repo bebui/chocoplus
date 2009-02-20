6ce843a7fbe3425ad4dc7ab63cb0cb399732761e
Julien Menana <jmenana@gmail.com>
2009-02-20 14:36:10 +0100

    new test

--- test.cc
+++ test.cc
@@ -12,18 +12,19 @@ int main()
   
 
   s.push();
-  IntVar a = s.make_var("a",10,20);
-  IntVar b = s.make_var("b",5,11);
+  IntVar a = s.make_var("a",10,12);
+  IntVar b = s.make_var("b",10,11);
   
-  Constraint c = s.eq(a,b);
+  //Constraint c = s.eq(a,b);
+  Constraint c2 = s.neq(a,b);
   
-  s.post(c);
-  cout << s.solve() << endl;
+  //s.post(c);
+  s.post(c2);
+  cout << ((s.solve()==0)?"NO SOLUTION" : "SOLVED") << endl;
+//  b.update_inf(10);
   cout << a.str() << endl << b.str() << endl; 
     
-  s.pop();
-  cout << endl;  
-  cout << a.str() << endl << b.str() << endl; 
+ 
   
   
   
